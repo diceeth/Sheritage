5a4e788195ad2cb906cff38aa5735834eca0b231
erwinphanglius <74586952+erwinphanglius@users.noreply.github.com>
2021-04-16 22:43:14 +0700

    Update AssetControl.test.js

--- test/AssetControl.test.js
+++ test/AssetControl.test.js
@@ -26,25 +26,40 @@ contract('AssetControl', accounts => {
         assert(parseFloat(result/1000000000000000000) === amountToken);
     });
 
+    it('Add User1 Success!', async() => {
+        console.log(account2);
+        const result = await assetControlContract.addUser(account2,50);
+    });
+
+    it('Add User2 Success!', async() => {
+        console.log(account3);
+        const result = await assetControlContract.addUser(account3,25);
+    });
+
+    it('Add User3 Success!', async() => {
+        console.log(account4);
+        const result = await assetControlContract.addUser(account4,25);
+    });
+
     it('Check BalanceOf Success!', async() => {
         const result = await assetControlContract.balanceOf(account1);
         assert(parseFloat(result/1000000000000000000) === 900);
     });
 
-    it('Relase Asset FirstAddr Success!', async() => {
-        await assetControlContract.releaseToken({from: account2});
+    it('Relase Asset User1 Success!', async() => {
+        await assetControlContract.releaseAsset({from: account2});
         const result = await assetControlContract.balanceOf(account2);
         assert(parseFloat(result/1000000000000000000) === 550);
     });
 
-    it('Relase Asset MiddleAddr Success!', async() => {
-        await assetControlContract.releaseToken({from: account3});
+    it('Relase Asset User2 Success!', async() => {
+        await assetControlContract.releaseAsset({from: account3});
         const result = await assetControlContract.balanceOf(account3);
         assert(parseFloat(result/1000000000000000000) === 225);
     });
 
-    it('Relase Asset LastAddr Success!', async() => {
-        await assetControlContract.releaseToken({from: account4});
+    it('Relase Asset User3 Success!', async() => {
+        await assetControlContract.releaseAsset({from: account4});
         const result = await assetControlContract.balanceOf(account4);
         assert(parseFloat(result/1000000000000000000) === 225);
     });
