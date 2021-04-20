3811706c06d3363b2fd8eceff19ae26a0c96d386
erwinphanglius <74586952+erwinphanglius@users.noreply.github.com>
2021-04-21 00:37:55 +0700

    Delete migrations directory

--- migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-const Migrations = artifacts.require("AssetControl.sol");
-
-module.exports = function (deployer) {
-  deployer.deploy(Migrations, 1618030769, 'tango');
-};
-
-// 1618030769 = 10 April
