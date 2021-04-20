ae836715ace9e5e5c6e1fa5761e3352f2bda1f05
erwinphanglius <74586952+erwinphanglius@users.noreply.github.com>
2021-04-21 00:12:46 +0700

    Update TokenWallet.sol

--- contracts/TokenWallet.sol
+++ contracts/TokenWallet.sol
@@ -2,22 +2,21 @@ pragma solidity 0.8.0;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
-contract TokenWallet is ERC20{
+contract FAMtoken is ERC20{
 
-    uint public initialPrice;
     uint public supplyToken;
     
     string private _name;
     string private _symbol;
     
     constructor() ERC20(_name, _symbol){
-        initialPrice = 1 ether;
         supplyToken = 1000;
         
         _name = 'FAMILY';
         _symbol = 'FAM';
-        _mint(msg.sender, supplyToken * (10 ** uint256(decimals())));
+        _mint(msg.sender, supplyToken * (1 ** uint256(decimals())));
     }
+    
     
     function name() public view virtual override returns (string memory) {
         return _name;
@@ -27,4 +26,7 @@ contract TokenWallet is ERC20{
         return _symbol;
     }
     
+    function transferToken(address recipient, uint256 amount) external payable{
+        _transfer(_msgSender(), recipient, amount);
+    }
 } 
