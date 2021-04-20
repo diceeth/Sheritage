b6ac51d2b9bfc6de55c2e29643a388a247be1b43
erwinphanglius <74586952+erwinphanglius@users.noreply.github.com>
2021-04-20 14:36:34 +0700

    Update 1_initial_migration.js

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,7 @@
 const Migrations = artifacts.require("AssetControl.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations, 1618030769, '0x22edbbee6D8DC5EdB22023aC6F8DF32b454e48c1', '0x23F242a08e3510f6BA1F7218E42cffFE62560D96', '0x11f1295ef4E4c220Ef04A95639d96927cE51b469', 50, 25, 25);
+  deployer.deploy(Migrations, 1618030769, 'tango');
 };
 
 // 1618030769 = 10 April
