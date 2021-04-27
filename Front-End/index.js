78c325a9683df05b33774db2daa5365fc7e2df95
erwinphanglius <74586952+erwinphanglius@users.noreply.github.com>
2021-04-27 15:26:29 +0700

    Update index.js

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,17 +1,13 @@
 if (window.ethereum) {
     window.web3 = new Web3(window.ethereum)
     try {
-        // ask user for permission
         ethereum.enable()
-        // user approved permission
     } catch (error) {
-        // user rejected permission
         console.log('user rejected permission')
     }
   }
   else if (window.web3) {
     window.web3 = new Web3(window.web3.currentProvider)
-    // no need to ask for permission
   }
   else {
     window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
